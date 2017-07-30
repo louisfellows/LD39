8d1811cd856bc1d6223ccca19b0ee4ded59301aa
Louis Fellows <louisfellows@gmail.com>
2017-07-30 20:44:58 +0100

    New Server.js

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
 
-console.log("Start");
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use('/assets',express.static(__dirname + '/assets'));
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
-app.use('/assets',express.static(__dirname + '/assets'));
 app.use('/lib',express.static(__dirname + '/lib'));
 
-console.log("Pages Served");
+// set the home page route
+app.get('/', function(req, res) {
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+// ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-console.log("get Index");
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 
-try {
-    server.listen(8080,function(){ // Listens to port 8081
-        console.log('Listening on '+server.address().port);
-    });
-} catch (err) {
-    console.log(err);
-}
 
-console.log("Running");
+
